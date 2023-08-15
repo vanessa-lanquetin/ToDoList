27265d3817a7a532646ed3bbf7fafa2064049ebe
vanessa-lanquetin <lanquetin.vanessa@gmail.com>
2023-08-15 21:39:42 +0200

    todolist finished

---
+++ index.js
@@ -0,0 +1,155 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const submitButton = document.getElementById("submitButton");
+  const list = document.getElementById("list");
+  const inputTask = document.getElementById("inputTask");
+
+  // Charger la liste depuis le stockage local lors du chargement de la page
+  loadListFromLocalStorage();
+
+  submitButton?.addEventListener("click", function () {
+    if (inputTask && inputTask instanceof HTMLInputElement) {
+      const inputValue = inputTask.value.trim(); // Supprimer les espaces inutiles
+
+      if (inputValue !== "") {
+        var task = document.createElement("li");
+
+        const taskText = document.createElement("span");
+        taskText.innerText = inputValue;
+
+        const statusSpan = document.createElement("span"); // Créer l'élément <span> pour le statut
+        statusSpan.classList.add("status"); // Ajouter une classe pour le sélectionner facilement
+
+        const trashIcon = document.createElement("i");
+        trashIcon.classList.add("fas", "fa-trash-alt");
+        trashIcon.addEventListener("click", () => {
+          task.remove();
+          saveListToLocalStorage(); // Mettre à jour le stockage local après suppression
+        });
+
+        task.appendChild(taskText); // Ajouter le span pour le nom de la tâche
+        task.appendChild(document.createTextNode(" "));
+        task.appendChild(statusSpan); // Ajouter le span du statut
+        task.appendChild(document.createTextNode(" "));
+        task.appendChild(trashIcon);
+
+        task.addEventListener("click", () => {
+          if (task.classList.contains("todo")) {
+            task.classList.replace("todo", "selected");
+            statusSpan.innerText = "Statut: En cours";
+          } else if (task.classList.contains("selected")) {
+            task.classList.replace("selected", "finished");
+            statusSpan.innerText = "Statut: Terminé";
+          } else if (task.classList.contains("finished")) {
+            task.classList.remove("finished");
+            statusSpan.innerText = "Statut: A faire";
+            task.classList.add("todo");
+          } else {
+            task.classList.add("todo");
+            statusSpan.innerText = "Statut: A faire";
+          }
+          saveListToLocalStorage(); // Mettre à jour le stockage local après modification
+        });
+
+        list?.appendChild(task);
+        saveListToLocalStorage(); // Mettre à jour le stockage local après ajout
+
+        inputTask.value = ""; // Réinitialiser l'input après l'ajout
+      }
+    } else {
+      console.error("Element with ID 'inputTask' not found.");
+    }
+  });
+
+  // Fonction pour sauvegarder la liste dans le stockage local
+  function saveListToLocalStorage() {
+    const tasks = Array.from(list.children).map((task) => {
+      const taskText = task.querySelector("span:not(.status)"); // Sélectionner le span pour le nom de la tâche
+      return {
+        text: taskText?.textContent,
+        isSelected: task.classList.contains("selected"),
+        isFinished: task.classList.contains("finished"),
+        isTodo: task.classList.contains("todo"), // Ajouter l'état "todo"
+      };
+    });
+
+    localStorage.setItem("taskList", JSON.stringify(tasks));
+  }
+
+  // Fonction pour charger la liste depuis le stockage local
+function loadListFromLocalStorage() {
+  // Récupérer les données de la liste de tâches depuis le stockage local
+  const savedTasks = localStorage.getItem("taskList");
+
+  // Vérifier si des tâches sont enregistrées dans le stockage local
+  if (savedTasks) {
+    // Parser les données JSON pour obtenir la liste des tâches
+    const tasks = JSON.parse(savedTasks);
+
+    // Parcourir chaque tâche dans la liste
+    tasks.forEach((taskInfo) => {
+      // Créer un élément <li> pour représenter la tâche
+      var task = document.createElement("li");
+
+      // Créer un élément <span> pour afficher le nom de la tâche
+      const taskText = document.createElement("span");
+      taskText.innerText = taskInfo.text;
+
+      // Créer un élément <span> pour afficher le statut de la tâche
+      const statusSpan = document.createElement("span");
+      statusSpan.classList.add("status"); // Ajouter une classe pour le style
+
+      // Vérifier le statut de la tâche et mettre à jour les classes et le texte du statut en conséquence
+      if (taskInfo.isSelected) {
+        task.classList.add("selected");
+        statusSpan.innerText = "Statut: En cours";
+      }
+      if (taskInfo.isFinished) {
+        task.classList.add("finished");
+        statusSpan.innerText = "Statut: Terminé";
+      }
+      if (taskInfo.isTodo) {
+        task.classList.add("todo");
+        statusSpan.innerText = "Statut: A faire";
+      }
+
+      // Créer une icône de corbeille pour supprimer la tâche
+      const trashIcon = document.createElement("i");
+      trashIcon.classList.add("fas", "fa-trash-alt");
+      trashIcon.addEventListener("click", () => {
+        task.remove(); // Supprimer la tâche du DOM
+        saveListToLocalStorage(); // Mettre à jour le stockage local
+      });
+
+      // Ajouter les éléments au DOM
+      task.appendChild(taskText); // Nom de la tâche
+      task.appendChild(document.createTextNode(" "));
+      task.appendChild(statusSpan); // Statut de la tâche
+      task.appendChild(document.createTextNode(" "));
+      task.appendChild(trashIcon); // Icône de corbeille
+
+      // Ajouter un gestionnaire d'événement pour changer le statut de la tâche au clic
+      task.addEventListener("click", () => {
+        if (task.classList.contains("todo")) {
+          task.classList.replace("todo", "selected");
+          statusSpan.innerText = "Statut: En cours";
+        } else if (task.classList.contains("selected")) {
+          task.classList.replace("selected", "finished");
+          statusSpan.innerText = "Statut: Terminé";
+        } else if (task.classList.contains("finished")) {
+          task.classList.remove("finished");
+          statusSpan.innerText = "Statut: A faire";
+          task.classList.add("todo");
+        } else {
+          task.classList.add("todo");
+          statusSpan.innerText = "Statut: A faire";
+        }
+        saveListToLocalStorage(); // Mettre à jour le stockage local
+      });
+
+      // Ajouter la tâche au conteneur de la liste
+      list?.appendChild(task);
+    });
+  }
+}
+
+});
